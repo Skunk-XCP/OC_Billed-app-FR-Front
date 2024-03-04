a0e062617cb5cf9441587acd328741b3c1f044aa
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-03-04 10:27:31 +0100

    Refacto Tests

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -89,158 +89,168 @@ describe("Given I am connected as an employee", () => {
    });
 });
 
-describe("When I am on BillUI and I click on new bill button", () => {
-   // Vérifie si la fonction `handleClickNewBill` est appelée lorsqu'on clique sur le bouton
-   test("Checks whether handleClickNewBill is called when the button is clicked", () => {
-      const mockNavigate = jest.fn();
+describe("Given I am connected as an employee", () => {
+   describe("When I am on BillUI and I click on new bill button", () => {
+      // Vérifie si la fonction `handleClickNewBill` est appelée lorsqu'on clique sur le bouton
+      test("Checks whether handleClickNewBill is called when the button is clicked", () => {
+         const mockNavigate = jest.fn();
 
-      // Création d'une nouvelle instance de l'objet Bills
-      const bills = new Bills({
-         document: document,
-         onNavigate: mockNavigate,
-         store: null,
-         localStorage: null,
-      });
-
-      document.body.innerHTML =
-         "<button data-testid='btn-new-bill'>New Bill</button>";
-
-      bills.handleClickNewBill();
-
-      // Vérifie que `handleClickNewBill` a bien été appelée avec l'élément `newBillButton` comme argument
-      expect(mockNavigate).toHaveBeenCalledWith(ROUTES_PATH.NewBill);
-   });
-});
-
-//*
-describe("When I am on Bills Page and I click on an eye icon", () => {
-   test("Checks whether handleClickIconEye is called when an icon is clicked", () => {
-      // On initialise l'élément div "icon-eye"
-      document.body.innerHTML = BillsUI({
-         data: bills,
-      });
-
-      const billsData = new Bills({
-         document,
-         onNavigate,
-         store: null,
-         localStorage: window.localStorage,
-      });
-
-      const modal = document.getElementById("modaleFile");
-
-      $.fn.modal = jest.fn(() => modal.classList.add("show"));
-
-      // Sélectionne le div représentant l'icône à partir du DOM simulé
-      const iconEye = screen.getAllByTestId("icon-eye")[0];
-
-      // Simule l'appel de handleClickIconEye avec iconEye pour tester sa réaction lors du clic sur l'icône.
-      const handleClickIconEye = jest.fn(billsData.handleClickIconEye(iconEye));
-
-      // Attache un gestionnaire d'événements de clic à l'icône
-      iconEye.addEventListener("click", handleClickIconEye);
-
-      // Simule un clic sur l'icône
-      fireEvent.click(iconEye);
-
-      // Vérifie que `handleClickIconEye` a bien été appelée avec l'élément `iconEye` comme argument
-      expect(handleClickIconEye).toHaveBeenCalled();
-   });
-});
-
-// test d'intégration GET Bills
-describe("Given I am a user connected as Employee", () => {
-   describe("When I navigate to Bill", () => {
-      // Test pour la récupération des factures via l'API mock
-      test("fetches bills from mock API GET", async () => {
-         localStorage.setItem(
-            "user",
-            JSON.stringify({ type: "Employee", email: "a@a" })
-         );
-
-         // configure le routage
-         const root = document.createElement("div");
-         root.setAttribute("id", "root");
-         document.body.append(root);
-         router();
-
-         // Simule la navigation vers la page des factures
-         window.onNavigate(ROUTES_PATH.Bills);
-
-         // Attends que le composant des factures soit présent dans le document
-         await waitFor(() => {
-            expect(screen.getByTestId("tbody")).toBeInTheDocument();
+         // Création d'une nouvelle instance de l'objet Bills
+         const bills = new Bills({
+            document: document,
+            onNavigate: mockNavigate,
+            store: null,
+            localStorage: null,
          });
 
-         // Sélectionne toutes les factures dans le corps du tableau
-         const billsData = screen.getByTestId("tbody").querySelectorAll("tr");
+         document.body.innerHTML =
+            "<button data-testid='btn-new-bill'>New Bill</button>";
 
-         // Vérifie que les éléments récupérés sont bien présents dans le DOM
-         expect(billsData).toBeTruthy();
+         bills.handleClickNewBill();
+
+         // Vérifie que `handleClickNewBill` a bien été appelée avec l'élément `newBillButton` comme argument
+         expect(mockNavigate).toHaveBeenCalledWith(ROUTES_PATH.NewBill);
       });
+   });
+});
 
-      test("When an error occurs on API", async () => {
-         beforeEach(() => {
-            // Surveille les appels faits à la méthode bills
-            jest.spyOn(mockStore, "bills");
+describe("Given I am connected as an employee", () => {
+   describe("When I am on Bills Page and I click on an eye icon", () => {
+      test("Checks whether handleClickIconEye is called when an icon is clicked", () => {
+         // On initialise l'élément div "icon-eye"
+         document.body.innerHTML = BillsUI({
+            data: bills,
+         });
 
-            // Configure les propriétés locales pour simuler un utilisateur connecté
-            Object.defineProperty(window, "localStorage", {
-               value: localStorageMock,
-            });
+         const billsData = new Bills({
+            document,
+            onNavigate,
+            store: null,
+            localStorage: window.localStorage,
+         });
 
-            window.localStorage.setItem(
+         const modal = document.getElementById("modaleFile");
+
+         $.fn.modal = jest.fn(() => modal.classList.add("show"));
+
+         // Sélectionne le div représentant l'icône à partir du DOM simulé
+         const iconEye = screen.getAllByTestId("icon-eye")[0];
+
+         // Simule l'appel de handleClickIconEye avec iconEye pour tester sa réaction lors du clic sur l'icône.
+         const handleClickIconEye = jest.fn(
+            billsData.handleClickIconEye(iconEye)
+         );
+
+         // Attache un gestionnaire d'événements de clic à l'icône
+         iconEye.addEventListener("click", handleClickIconEye);
+
+         // Simule un clic sur l'icône
+         fireEvent.click(iconEye);
+
+         // Vérifie que `handleClickIconEye` a bien été appelée avec l'élément `iconEye` comme argument
+         expect(handleClickIconEye).toHaveBeenCalled();
+      });
+   });
+});
+
+describe("Given I am connected as an employee", () => {
+   // test d'intégration GET Bills
+   describe("Given I am a user connected as Employee", () => {
+      describe("When I navigate to Bill", () => {
+         // Test pour la récupération des factures via l'API mock
+         test("fetches bills from mock API GET", async () => {
+            localStorage.setItem(
                "user",
-               JSON.stringify({
-                  type: "Employee",
-                  email: "a@a",
-               })
+               JSON.stringify({ type: "Employee", email: "a@a" })
             );
 
             // configure le routage
             const root = document.createElement("div");
             root.setAttribute("id", "root");
-            document.body.appendChild(root);
+            document.body.append(root);
             router();
-         });
-      });
-      // Vérifie la récupération des données depuis l'API qui échoue avec erreur 404
-      test("fetches bills from an API and fails with 404 message error", async () => {
-         mockStore.bills.mockImplementationOnce(() => {
-            return {
-               list: () => {
-                  return Promise.reject(new Error("Erreur 404"));
-               },
-            };
-         });
 
-         //Navige vers la page Bills
-         window.onNavigate(ROUTES_PATH.Bills);
+            // Simule la navigation vers la page des factures
+            window.onNavigate(ROUTES_PATH.Bills);
 
-         // Attend la fin du cycle d'événement courant
-         await new Promise(process.nextTick);
+            // Attends que le composant des factures soit présent dans le document
+            await waitFor(() => {
+               expect(screen.getByTestId("tbody")).toBeInTheDocument();
+            });
 
-         // Recherche dans le DOM un élément contenant le texte "Erreur 404"
-         const message = await screen.getByText(/Erreur 404/);
+            // Sélectionne toutes les factures dans le corps du tableau
+            const billsData = screen
+               .getByTestId("tbody")
+               .querySelectorAll("tr");
 
-         // Vérifie que l'élément trouvé existe bien
-         expect(message).toBeTruthy();
-      });
-
-      // Vérifie la récupération des données depuis l'API qui échoue avec erreur 500
-      test("fetches messages from an API and fails with 500 message error", async () => {
-         mockStore.bills.mockImplementationOnce(() => {
-            return {
-               list: () => {
-                  return Promise.reject(new Error("Erreur 500"));
-               },
-            };
+            // Vérifie que les éléments récupérés sont bien présents dans le DOM
+            expect(billsData).toBeTruthy();
          });
 
-         window.onNavigate(ROUTES_PATH.Bills);
-         await new Promise(process.nextTick);
-         const message = await screen.getByText(/Erreur 500/);
-         expect(message).toBeTruthy();
+         test("When an error occurs on API", async () => {
+            beforeEach(() => {
+               // Surveille les appels faits à la méthode bills
+               jest.spyOn(mockStore, "bills");
+
+               // Configure les propriétés locales pour simuler un utilisateur connecté
+               Object.defineProperty(window, "localStorage", {
+                  value: localStorageMock,
+               });
+
+               window.localStorage.setItem(
+                  "user",
+                  JSON.stringify({
+                     type: "Employee",
+                     email: "a@a",
+                  })
+               );
+
+               // configure le routage
+               const root = document.createElement("div");
+               root.setAttribute("id", "root");
+               document.body.appendChild(root);
+               router();
+            });
+         });
+
+         // Vérifie la récupération des données depuis l'API qui échoue avec erreur 404
+         test("fetches bills from an API and fails with 404 message error", async () => {
+            mockStore.bills.mockImplementationOnce(() => {
+               return {
+                  list: () => {
+                     return Promise.reject(new Error("Erreur 404"));
+                  },
+               };
+            });
+
+            //Navige vers la page Bills
+            window.onNavigate(ROUTES_PATH.Bills);
+
+            // Attend la fin du cycle d'événement courant
+            await new Promise(process.nextTick);
+
+            // Recherche dans le DOM un élément contenant le texte "Erreur 404"
+            const message = await screen.getByText(/Erreur 404/);
+
+            // Vérifie que l'élément trouvé existe bien
+            expect(message).toBeTruthy();
+         });
+
+         // Vérifie la récupération des données depuis l'API qui échoue avec erreur 500
+         test("fetches messages from an API and fails with 500 message error", async () => {
+            mockStore.bills.mockImplementationOnce(() => {
+               return {
+                  list: () => {
+                     return Promise.reject(new Error("Erreur 500"));
+                  },
+               };
+            });
+
+            window.onNavigate(ROUTES_PATH.Bills);
+            await new Promise(process.nextTick);
+            const message = await screen.getByText(/Erreur 500/);
+            expect(message).toBeTruthy();
+         });
       });
    });
 });
